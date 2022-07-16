a157755cc85362ff268f00ef93d97c4c478af99f
Rodhis <rodrigonoliva@gmail.com>
2022-07-16 18:23:55 -0300

    changing file names to english

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const [h,m,s] = [d.getHours(), d.getMinutes(), d.getSeconds()]
 msg.innerHTML = `Agora são ${h} hora${(h>1)?"s":""}, ${m} minuto${(m>1)?"s":""} e ${s} segundo${(s>1)?"s":""}!`
 
 if (h >= 6 && h < 12) {
-    img.src = "manha.png"
+    img.src = "morning.png"
     document.body.style.background = '#8493a9'
 
 } else if (h >= 12 && h < 18) {
-    img.src = "tarde.png"
+    img.src = "afternoon.png"
     document.body.style.background = '#fe9302'
 
 } else {
-    img.src = "noite.png"
+    img.src = "night.png"
     document.body.style.background = '#182b4b'
 }
 }
